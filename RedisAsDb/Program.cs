63421c20e3985d87dcb6b07895965a259b33ef07
UNIVERSE\aillarionov <illarionoff.a@gmail.com>
2023-03-07 20:56:44 +0100

    Connect to redis

--- RedisAsDb/Program.cs
+++ RedisAsDb/Program.cs
@@ -1,4 +1,8 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(opt => ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("RedisConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
